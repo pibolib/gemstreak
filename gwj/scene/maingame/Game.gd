59a95f925780f51eff87f5d8df3b77e7d60e0e6b
Jono953 <jonoschool953@gmail.com>
2021-07-09 21:26:51 -0400

    finally fixed the god dang jank i swear to god

--- gwj/scene/maingame/Game.gd
+++ gwj/scene/maingame/Game.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var fallingtile = preload("res://scene/maingame/fallingtile/FallingTile.tscn")
+
 #game variables
 var score = 0
 
@@ -10,11 +12,29 @@ var currenttri = [0,0,0]
 
 var garbagelevel = 0
 
+var time = 0
+var test = false
+
 func _ready():
 	pass
-
+	
 func _process(delta):
+	time += delta
 	update_info()
+	if time > 0.25 and !test:
+		var mytile = fallingtile.instance()
+		mytile.position = Vector2(0,0)
+		mytile.type = 3
+		$Board.add_child(mytile)
+		var mytile2 = fallingtile.instance()
+		mytile2.position = Vector2(16,0)
+		mytile2.type = 3
+		$Board.add_child(mytile2)
+		var mytile3 = fallingtile.instance()
+		mytile3.position = Vector2(32,0)
+		mytile3.type = 3
+		$Board.add_child(mytile3)
+		test = true
 
 func update_info():
 	$Info.text = "Score: "+String(score)+"\nStreak: "+String(streak)+" ("+String(streaktime)+")\nGarbage Level: "+String(garbagelevel)
---
+++ gwj/scene/maingame/fallingtile/FallingTile.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+
+const G = 4
+var type = 0
+var poslastframe = Vector2(0,0)
+var motion = Vector2(0,1)
+var gravity = Vector2(0,G)
+var spritetable = [
+	Vector2(0,0),
+	Vector2(16,0),
+	Vector2(32,0),
+	Vector2(48,0)
+]
+var settletime = 0
+var tilepos = Vector2(int(0),int(0))
+
+func _ready():
+	if type == -1:
+		queue_free()
+	else:
+		$Sprite.region_rect.position = spritetable[type]
+	calculate_tilepos()
+	print(tilepos)
+
+func _physics_process(delta):
+	calculate_tilepos()
+	motion += gravity
+	motion = move_and_slide(motion,Vector2(0,-1),false,4,0.785398,false)
+	if abs(position.x-poslastframe.x) < 1 and abs(position.y-poslastframe.y) < 1:
+		settletime += delta
+	else:
+		settletime = 0
+	if settletime >= 0.5:
+		calculate_tilepos()
+		get_parent().set_cell(tilepos.x,tilepos.y,type)
+		print("h at "+String(tilepos))
+		queue_free()
+	poslastframe = position
+func calculate_tilepos():
+	var intpos = [int(position.x),int(position.y)]
+	if intpos[1] % 16 < 8:
+		tilepos.x = intpos[0]/16
+	else:
+		tilepos.x = int(intpos[0]/16)
+	tilepos.y = intpos[1]/8
